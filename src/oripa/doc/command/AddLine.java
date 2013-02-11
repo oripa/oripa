5fe6ccae1d94b265b5b4829deae56c6b0d20b785
Ooouch1 <origamist.koji@gmail.com>
2013-02-11 22:20:41 +0900

    Fix "add line" bug I found existing lines would not be separated. That is my mistake.

--- src/oripa/doc/command/AddLine.java
+++ src/oripa/doc/command/AddLine.java
@@ -6,6 +6,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
 
 import javax.vecmath.Vector2d;
 
@@ -38,28 +39,23 @@ public class AddLine {
 
 
 	/**
-	 * Specified procedure for adding aux line.
+	 * 
 	 * @param inputLine
 	 * @param currentLines
-	 * @return true if the line is aux.
+	 * @return true.
 	 */
-	public boolean addAuxLine(OriLine inputLine, Collection<OriLine> currentLines){
-
-		if(inputLine.typeVal != OriLine.TYPE_NONE){
-			return false;
-		}
+	public boolean divideCurrentLines(OriLine inputLine, Collection<OriLine> currentLines){
 
 		LinkedList<OriLine> toBeAdded = new LinkedList<>();
 
-		// If it intersects other line, divide them
+		// intersection of (aux input, other type lines) are rejected
 		for (Iterator<OriLine> iterator = currentLines.iterator(); iterator.hasNext();) {
 			OriLine line = iterator.next();
 
-
-			// Inputted line does not intersect
-			if (line.typeVal != OriLine.TYPE_NONE) {
-				continue;
-			}
+            if (inputLine.typeVal == OriLine.TYPE_NONE && line.typeVal != OriLine.TYPE_NONE) {
+                continue;
+            }
+            
 			Vector2d crossPoint = GeomUtil.getCrossPoint(inputLine, line);
 			if (crossPoint == null) {
 				continue;
@@ -86,6 +82,52 @@ public class AddLine {
 	}
 
 	/**
+	 * Input line should be divided by other lines. This function returns end points of such new small lines.
+	 * @param inputLine
+	 * @param currentLines
+	 * @return points on input line divided by currentLines
+	 */
+	public List<Vector2d> createInputLinePoints(OriLine inputLine, Collection<OriLine> currentLines){
+		ArrayList<Vector2d> points = new ArrayList<Vector2d>();
+		points.add(inputLine.p0);
+		points.add(inputLine.p1);
+		
+		// divide input line by existing lines
+		for (OriLine line : currentLines) {
+
+			// reject (M/V input, aux lines) // It is by MITANI jun, I don't know why.
+			if (inputLine.typeVal != OriLine.TYPE_NONE && 
+					line.typeVal == OriLine.TYPE_NONE) {
+				continue;
+			}
+
+			// If the intersection is on the end of the line, skip
+			if (GeomUtil.Distance(inputLine.p0, line.p0) < CalculationResource.POINT_EPS ||
+					GeomUtil.Distance(inputLine.p0, line.p1) < CalculationResource.POINT_EPS||
+					GeomUtil.Distance(inputLine.p1, line.p0) < CalculationResource.POINT_EPS||
+					GeomUtil.Distance(inputLine.p1, line.p1) < CalculationResource.POINT_EPS) {
+				continue;
+			}
+
+			if (GeomUtil.DistancePointToSegment(line.p0, inputLine.p0, inputLine.p1) < CalculationResource.POINT_EPS) {
+				points.add(line.p0);
+			}
+			if (GeomUtil.DistancePointToSegment(line.p1, inputLine.p0, inputLine.p1) < CalculationResource.POINT_EPS) {
+				points.add(line.p1);
+			}
+
+			// Calculates the intersection
+			Vector2d crossPoint = GeomUtil.getCrossPoint(inputLine, line);
+			if (crossPoint != null) {
+				points.add(crossPoint);
+			}
+
+		}
+
+		return points;
+	}
+	
+	/**
 	 * Adds a new OriLine, also searching for intersections with others 
 	 * that would cause their mutual division
 	 * 
@@ -97,9 +139,6 @@ public class AddLine {
 	public void addLine(OriLine inputLine, Collection<OriLine> currentLines) {
 		//ArrayList<OriLine> crossingLines = new ArrayList<OriLine>(); // for debug? 
 
-		ArrayList<Vector2d> points = new ArrayList<Vector2d>();
-		points.add(inputLine.p0);
-		points.add(inputLine.p1);
 
 		// If it already exists, do nothing
 		for (OriLine line : currentLines) {
@@ -108,43 +147,11 @@ public class AddLine {
 			}
 		}
 
-		if (inputLine.typeVal == OriLine.TYPE_NONE){
-			// for the case of aux input
-			addAuxLine(inputLine, currentLines);
-		}
-		else{   
-			for (OriLine line : currentLines) {
-
-				// Dont devide if the type of line is aux is Aux
-				if (//inputLine.typeVal != OriLine.TYPE_NONE && 
-						line.typeVal == OriLine.TYPE_NONE) {
-					continue;
-				}
-
-				// If the intersection is on the end of the line, skip
-				if (GeomUtil.Distance(inputLine.p0, line.p0) < CalculationResource.POINT_EPS ||
-						GeomUtil.Distance(inputLine.p0, line.p1) < CalculationResource.POINT_EPS||
-						GeomUtil.Distance(inputLine.p1, line.p0) < CalculationResource.POINT_EPS||
-						GeomUtil.Distance(inputLine.p1, line.p1) < CalculationResource.POINT_EPS) {
-					continue;
-				}
-
-				if (GeomUtil.DistancePointToSegment(line.p0, inputLine.p0, inputLine.p1) < CalculationResource.POINT_EPS) {
-					points.add(line.p0);
-				}
-				if (GeomUtil.DistancePointToSegment(line.p1, inputLine.p0, inputLine.p1) < CalculationResource.POINT_EPS) {
-					points.add(line.p1);
-				}
-
-				// Calculates the intersection
-				Vector2d crossPoint = GeomUtil.getCrossPoint(inputLine, line);
-				if (crossPoint != null) {
-					points.add(crossPoint);
-				}
-
-			}
-		}
-		// the sort is done on longer direction in order to suppress underflow error???
+		divideCurrentLines(inputLine, currentLines);
+		
+		List<Vector2d> points = createInputLinePoints(inputLine, currentLines);
+		
+		// sort in order to make points sequential
 		boolean sortByX = Math.abs(inputLine.p0.x - inputLine.p1.x) > Math.abs(inputLine.p0.y - inputLine.p1.y);
 		if (sortByX) {
 			Collections.sort(points, new PointComparatorX());
