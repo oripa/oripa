1f2c6899f8fee37c3eb26fc5f5e37947519bd421
origami ouch <origamist.koji@gmail.com>
2019-10-17 01:22:25 +0900

    Refactoring: clear selections on recovering by-value action state.

--- src/main/java/oripa/domain/paint/byvalue/LineByValueAction.java
+++ src/main/java/oripa/domain/paint/byvalue/LineByValueAction.java
@@ -13,57 +13,67 @@ import oripa.domain.paint.util.ElementSelector;
 
 public class LineByValueAction extends GraphicMouseAction {
 
-	
-	public LineByValueAction(){
+	public LineByValueAction() {
 		setActionState(new SelectingVertexToDrawLine());
 	}
-	
+
 	@Override
-	public void onDrag(PaintContextInterface context, AffineTransform affine,
-			boolean differentAction) {
+	public void onDrag(final PaintContextInterface context, final AffineTransform affine,
+			final boolean differentAction) {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public void onRelease(PaintContextInterface context, AffineTransform affine,
-			boolean differentAction) {
+	public void onRelease(final PaintContextInterface context, final AffineTransform affine,
+			final boolean differentAction) {
 		// TODO Auto-generated method stub
 
 	}
 
+	/*
+	 * (non Javadoc)
+	 *
+	 * @see
+	 * oripa.domain.paint.core.GraphicMouseAction#recover(oripa.domain.paint.
+	 * PaintContextInterface)
+	 */
 	@Override
-	public void onDraw(Graphics2D g2d, PaintContextInterface context) {
+	public void recover(final PaintContextInterface context) {
+		context.clear(true);
+	}
+
+	@Override
+	public void onDraw(final Graphics2D g2d, final PaintContextInterface context) {
 		super.onDraw(g2d, context);
-		
+
 		drawPickCandidateVertex(g2d, context);
-        Vector2d v = context.getCandidateVertexToPick();
-		if(v != null){
-	        try {
-	            double length = ValueDB.getInstance().getLength();
-	            double angle = ValueDB.getInstance().getAngle();
-	
-	            angle = Math.toRadians(angle);
-	            
-	            ElementSelector selector = new ElementSelector();
-	            g2d.setColor(selector.selectColorByLineType(PaintConfig.inputLineType));
-	            g2d.setStroke(selector.selectStroke(PaintConfig.inputLineType));
-	            
-	            Vector2d dir = new Vector2d(Math.cos(angle), -Math.sin(angle));
-	            dir.scale(length);
-	            g2d.draw(new Line2D.Double(v.x, v.y, v.x + dir.x, v.y + dir.y));
-	        } catch (Exception e) {
-	            e.printStackTrace();
-	        }
+		Vector2d v = context.getCandidateVertexToPick();
+		if (v != null) {
+			try {
+				double length = ValueDB.getInstance().getLength();
+				double angle = ValueDB.getInstance().getAngle();
+
+				angle = Math.toRadians(angle);
+
+				ElementSelector selector = new ElementSelector();
+				g2d.setColor(selector.selectColorByLineType(PaintConfig.inputLineType));
+				g2d.setStroke(selector.selectStroke(PaintConfig.inputLineType));
+
+				Vector2d dir = new Vector2d(Math.cos(angle), -Math.sin(angle));
+				dir.scale(length);
+				g2d.draw(new Line2D.Double(v.x, v.y, v.x + dir.x, v.y + dir.y));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
 	@Override
-	public void onPress(PaintContextInterface context, AffineTransform affine,
-			boolean differentAction) {
+	public void onPress(final PaintContextInterface context, final AffineTransform affine,
+			final boolean differentAction) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	
 }
