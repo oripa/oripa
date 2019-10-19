ff8567b657bebd2c0d3c3bfa0bdc6d77d5036621
origami ouch <origamist.koji@gmail.com>
2019-10-20 01:23:25 +0900

    Bug fix: Adding vertex didn't work.

--- src/main/java/oripa/domain/cptool/LineDivider.java
+++ src/main/java/oripa/domain/cptool/LineDivider.java
@@ -11,29 +11,28 @@ import oripa.value.OriLine;
 public class LineDivider {
 
 	/**
-	 * 
+	 *
 	 * @param line
 	 * @param v
 	 * @param creasePattern
 	 * @param paperSize
-	 * @return null if not need to divides
+	 * @return collection containing 2 lines that are the result of division.
+	 *         null if not need to divides
 	 */
 	public Collection<OriLine> divideLineInCollection(
-			OriLine line, Vector2d v,
-			Collection<OriLine> creasePattern, double paperSize) {
+			final OriLine line, final Vector2d v,
+			final Collection<OriLine> creasePattern, final double paperSize) {
 		ArrayList<OriLine> divided = new ArrayList<>(2);
 
-		// Normally you don't want to add a vertex too close to the end of the line
+		// Normally you don't want to add a vertex too close to the end of the
+		// line
 		if (GeomUtil.Distance(line.p0, v) < paperSize * 0.001
 				|| GeomUtil.Distance(line.p1, v) < paperSize * 0.001) {
 			return null;
 		}
 
-		OriLine l0 = new OriLine(line.p0, v, line.typeVal);
-		OriLine l1 = new OriLine(v, line.p1, line.typeVal);
-//		creasePattern.remove(line);
-//		creasePattern.add(l0);
-//		creasePattern.add(l1);
+		divided.add(new OriLine(line.p0, v, line.typeVal));
+		divided.add(new OriLine(v, line.p1, line.typeVal));
 
 		return divided;
 	}
