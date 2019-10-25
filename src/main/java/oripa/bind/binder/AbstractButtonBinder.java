4ebe8d076874855eeecfeba763efc9484bd231d6
origami ouch <origamist.koji@gmail.com>
2019-10-25 20:56:56 +0900

    deprecated newInstance() are avoided.

--- src/main/java/oripa/bind/binder/AbstractButtonBinder.java
+++ src/main/java/oripa/bind/binder/AbstractButtonBinder.java
@@ -2,48 +2,47 @@ package oripa.bind.binder;
 
 import javax.swing.AbstractButton;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import oripa.resource.ResourceHolder;
 import oripa.resource.ResourceKey;
 
-public abstract class AbstractButtonBinder<ToBeBound> 
-implements BinderInterface<ToBeBound>{
+public abstract class AbstractButtonBinder<ToBeBound>
+		implements BinderInterface<ToBeBound> {
 
-	
+	private static Logger logger = LoggerFactory.getLogger(AbstractButtonBinder.class);
+
 	/**
-	 * 
+	 *
 	 * @param buttonClass
-	 * @param textID StringID member for label
+	 * @param textID
+	 *            StringID member for label
 	 * @return
 	 */
 	protected AbstractButton createEmptyButton(
-			Class<? extends AbstractButton> buttonClass, String textID) {
+			final Class<? extends AbstractButton> buttonClass, final String textID) {
 
 		ResourceHolder resourceHolder = ResourceHolder.getInstance();
-	
+
 		/*
 		 * construct button
 		 */
 		AbstractButton button = null;
 		try {
-			button = buttonClass.newInstance();
-		} catch (InstantiationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			button = buttonClass.getConstructor().newInstance();
+		} catch (Exception e) {
+			logger.error("failed to create a button", e);
 		}
-	
+
 		/*
 		 * set label
 		 */
-		try{
+		try {
 			button.setText(resourceHolder.getString(ResourceKey.LABEL, textID));
+		} catch (Exception e) {
+			logger.error("failed to set a label to button", e);
 		}
-		catch (Exception e) {
-		}
-	
-		
 
 		return button;
 	}
--- src/main/java/oripa/util/collection/CollectionCloneFactory.java
+++ src/main/java/oripa/util/collection/CollectionCloneFactory.java
@@ -1,25 +1,31 @@
 package oripa.util.collection;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.Collection;
 
 public class CollectionCloneFactory<Element> {
 
 	/**
 	 * creates clone of given collection as specified class
+	 *
 	 * @param list
 	 * @param returnedType
 	 * @return
 	 * @throws InstantiationException
 	 * @throws IllegalAccessException
+	 * @throws InvocationTargetException
+	 * @throws NoSuchMethodException
 	 */
 	public Collection<Element> createCloneOf(
-			Collection<Element> list, Class<? extends Collection> returnedType)
-					throws InstantiationException, IllegalAccessException {
+			final Collection<Element> list, final Class<? extends Collection> returnedType)
+			throws InstantiationException, IllegalAccessException, IllegalArgumentException,
+			InvocationTargetException, NoSuchMethodException {
 
-		Collection<Element> cloned = returnedType.newInstance();
-		
+		@SuppressWarnings("unchecked")
+		Collection<Element> cloned = returnedType.getConstructor().newInstance();
+
 		cloned.addAll(list);
-		
+
 		return cloned;
 	}
 }
