e60499c4befbbebd4333ca306cd7b0312fc7ba20
origami ouch <origamist.koji@gmail.com>
2019-11-03 21:37:18 +0900

    Refactoring: apply Stream API.

--- src/main/java/oripa/domain/cptool/ElementRemover.java
+++ src/main/java/oripa/domain/cptool/ElementRemover.java
@@ -10,18 +10,20 @@ import oripa.value.OriLine;
 
 /**
  * This class defines how to remove line/vertex from crease pattern.
+ *
  * @author Koji
  *
  */
 public class ElementRemover {
 	/**
 	 * remove line from crease pattern
+	 *
 	 * @param l
 	 * @param creasePattern
 	 */
 	public void removeLine(
-			OriLine l, Collection<OriLine> creasePattern) {
-		
+			final OriLine l, final Collection<OriLine> creasePattern) {
+
 		creasePattern.remove(l);
 		// merge the lines if possible, to prevent unnecessary vertexes
 		merge2LinesAt(l.p0, creasePattern);
@@ -30,23 +32,25 @@ public class ElementRemover {
 
 	/**
 	 * remove vertex from crease pattern
+	 *
 	 * @param v
 	 * @param creasePattern
 	 */
 	public void removeVertex(
-			Vector2d v, Collection<OriLine> creasePattern) {
-		
+			final Vector2d v, final Collection<OriLine> creasePattern) {
+
 		merge2LinesAt(v, creasePattern);
 	}
+
 	private void merge2LinesAt(
-			Vector2d p, Collection<OriLine> creasePattern) {
-		
+			final Vector2d p, final Collection<OriLine> creasePattern) {
+
 		ArrayList<OriLine> sharedLines = new ArrayList<OriLine>();
-		for (OriLine line : creasePattern) {
-			if (GeomUtil.Distance(line.p0, p) < 0.001 || GeomUtil.Distance(line.p1, p) < 0.001) {
-				sharedLines.add(line);
-			}
-		}
+
+		creasePattern.stream()
+				.filter(line -> GeomUtil.Distance(line.p0, p) < 0.001
+						|| GeomUtil.Distance(line.p1, p) < 0.001)
+				.forEach(line -> sharedLines.add(line));
 
 		if (sharedLines.size() != 2) {
 			return;
@@ -93,21 +97,20 @@ public class ElementRemover {
 
 	/**
 	 * remove lines which is marked "selected" from given collection.
-	 * @param creasePattern collection of lines
+	 *
+	 * @param creasePattern
+	 *            collection of lines
 	 */
 	public void removeSelectedLines(
-			Collection<OriLine> creasePattern) {
+			final Collection<OriLine> creasePattern) {
 
 		ArrayList<OriLine> selectedLines = new ArrayList<OriLine>();
-		for (OriLine line : creasePattern) {
-			if (line.selected) {
-				selectedLines.add(line);
-			}
-		}
 
-		for (OriLine line : selectedLines) {
-			creasePattern.remove(line);
-		}
+		creasePattern.stream()
+				.filter(line -> line.selected)
+				.forEach(line -> selectedLines.add(line));
+
+		selectedLines.forEach(line -> creasePattern.remove(line));
 	}
 
 }
