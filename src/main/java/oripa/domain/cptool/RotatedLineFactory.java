6ea32936765388d7623ae907e7560506cbafd468
origami ouch <origamist.koji@gmail.com>
2019-11-03 22:52:04 +0900

    Apply Stream API to RotatedLineFactory.

--- src/main/java/oripa/domain/cptool/RotatedLineFactory.java
+++ src/main/java/oripa/domain/cptool/RotatedLineFactory.java
@@ -9,72 +9,70 @@ import oripa.value.OriPoint;
 public class RotatedLineFactory {
 
 	/**
-	 * create copy of selected lines with a rotation around specified center point.
-	 * For a line l, this method creates rotatedLine(l, angleDeg * i) for i = 1 ... repetitionCount.
-	 * 
-	 * @param cx       x of center
-	 * @param cy       y of center
-	 * @param angleDeg         amount of rotation in degrees
-	 * @param repetitionCount  
+	 * create copy of selected lines with a rotation around specified center
+	 * point. For a line l, this method creates rotatedLine(l, angleDeg * i) for
+	 * i = 1 ... repetitionCount.
+	 *
+	 * @param cx
+	 *            x of center
+	 * @param cy
+	 *            y of center
+	 * @param angleDeg
+	 *            amount of rotation in degrees
+	 * @param repetitionCount
 	 * @param paperSize
-	 * 
+	 *
 	 * @return rotated lines
 	 */
 	public Collection<OriLine> createRotatedLines(
-			double cx, double cy, double angleDeg, int repetitionCount,
-			Collection<OriLine> creasePattern, double paperSize) {
+			final double cx, final double cy, final double angleDeg, final int repetitionCount,
+			final Collection<OriLine> creasePattern, final double paperSize) {
 
-				
 		ArrayList<OriLine> rotatedLines = new ArrayList<OriLine>();
 
-
-		oripa.domain.paint.util.RectangleClipper clipper =
-				new oripa.domain.paint.util.RectangleClipper(
-						-paperSize / 2, -paperSize / 2, paperSize / 2, paperSize / 2);
+		oripa.domain.paint.util.RectangleClipper clipper = new oripa.domain.paint.util.RectangleClipper(
+				-paperSize / 2, -paperSize / 2, paperSize / 2, paperSize / 2);
 
 		double angle = angleDeg * Math.PI / 180.0;
 
 		for (int i = 0; i < repetitionCount; i++) {
 			double angleRad = angle * (i + 1);
-			for (OriLine l : creasePattern) {
-				if (!l.selected) {
-					continue;
-				}
 
+			creasePattern.stream().filter(l -> l.selected).forEach(l -> {
 				OriPoint center = new OriPoint(cx, cy);
 
 				OriPoint r0 = rotateAroundCenter(l.p0, center, angleRad);
 				OriPoint r1 = rotateAroundCenter(l.p1, center, angleRad);
-				
+
 				OriLine rotatedLine = new OriLine(r0, r1, l.getTypeValue());
 
 				if (clipper.clip(rotatedLine)) {
 					rotatedLines.add(rotatedLine);
 				}
-			}
+			});
 		}
 
 		return rotatedLines;
 	}
 
-	private OriPoint rotateAroundCenter(OriPoint p, OriPoint center, double angleRad) {
+	private OriPoint rotateAroundCenter(final OriPoint p, final OriPoint center,
+			final double angleRad) {
 
-		OriPoint shiftedToCenter =
-				new OriPoint(p.x - center.x, p.y - center.y);
+		OriPoint shiftedToCenter = new OriPoint(p.x - center.x, p.y - center.y);
 
 		OriPoint rotated = rotate(shiftedToCenter, angleRad);
 
 		rotated.add(center);
-		
+
 		return rotated;
 	}
-	
-	private OriPoint rotate(OriPoint p, double angleRad) {
+
+	private OriPoint rotate(final OriPoint p, final double angleRad) {
 		OriPoint rotated = new OriPoint();
-		
+
 		rotated.x = p.x * Math.cos(angleRad) - p.y * Math.sin(angleRad);
 		rotated.y = p.x * Math.sin(angleRad) + p.y * Math.cos(angleRad);
-		
+
 		return rotated;
 	}
 }
