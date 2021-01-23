9995bf0a8209bda36f43bd06af4aa5f7c28ca816
lifesbest23 <62232806+lifesbest23@users.noreply.github.com>
2021-01-23 20:26:56 +0100

    Update ResourceHolder.java
    removed empty line

--- src/main/java/oripa/resource/ResourceHolder.java
+++ src/main/java/oripa/resource/ResourceHolder.java
@@ -49,7 +49,6 @@ public class ResourceHolder {
 				createResource(resourcePackage + ".ErrorStringResource_en"));
 		holder.addResource(ResourceKey.APP_INFO,
 				createResource(resourcePackage + ".AppInfoResource"));
-
 	}
 	
 	private ResourceBundle createResource(final String classPath) {
