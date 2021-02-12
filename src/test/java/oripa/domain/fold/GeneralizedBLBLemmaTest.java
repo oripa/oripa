c1e96470c3ce272292777129e4bd81abd19aa89b
origami ouch <origamist.koji@gmail.com>
2021-02-13 01:18:37 +0900

    Add test of generalized BLB lemma.

---
+++ src/test/java/oripa/domain/fold/GeneralizedBLBLemmaTest.java
@@ -0,0 +1,135 @@
+/**
+ * ORIPA - Origami Pattern Editor
+ * Copyright (C) 2013-     ORIPA OSS Project  https://github.com/oripa/oripa
+ * Copyright (C) 2005-2009 Jun Mitani         http://mitani.cs.tsukuba.ac.jp/
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package oripa.domain.fold;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import oripa.value.OriLine;
+
+/**
+ * @author OUCHI Koji
+ *
+ */
+class GeneralizedBLBLemmaTest {
+
+	/**
+	 * Test method for
+	 * {@link oripa.domain.fold.GeneralizedBigLittleBigLemma#holds(oripa.domain.fold.OriVertex)}.
+	 */
+	@Test
+	void testHolds_birdFoot() {
+		var oriVertex = createBirdFoot();
+		var blb = new GeneralizedBigLittleBigLemma();
+
+		assertTrue(blb.holds(oriVertex));
+
+		oriVertex.edges.get(1).type = OriLine.Type.MOUNTAIN.toInt();
+		oriVertex.edges.get(3).type = OriLine.Type.VALLEY.toInt();
+
+		assertFalse(blb.holds(oriVertex));
+	}
+
+	private OriVertex createBirdFoot() {
+		var oriVertex = new OriVertex(1, 1);
+		oriVertex.addEdge(createEdge(0, 0, 1, 1, OriLine.Type.MOUNTAIN));
+		oriVertex.addEdge(createEdge(1, 0, 1, 1, OriLine.Type.VALLEY));
+		oriVertex.addEdge(createEdge(2, 0, 1, 1, OriLine.Type.MOUNTAIN));
+
+		oriVertex.addEdge(createEdge(1, 2, 1, 1, OriLine.Type.MOUNTAIN));
+
+		return oriVertex;
+	}
+
+	@Test
+	void testHolds_equqlAngles() {
+		var oriVertex = createEqualAngles();
+		var blb = new GeneralizedBigLittleBigLemma();
+
+		assertTrue(blb.holds(oriVertex));
+
+	}
+
+	private OriVertex createEqualAngles() {
+		var oriVertex = new OriVertex(1, 1);
+		oriVertex.addEdge(createEdge(0, 1, 1, 1, OriLine.Type.MOUNTAIN));
+		oriVertex.addEdge(createEdge(1, 0, 1, 1, OriLine.Type.VALLEY));
+		oriVertex.addEdge(createEdge(2, 1, 1, 1, OriLine.Type.MOUNTAIN));
+		oriVertex.addEdge(createEdge(1, 2, 1, 1, OriLine.Type.MOUNTAIN));
+
+		return oriVertex;
+	}
+
+	@Test
+	void testHolds_twoSequences() {
+		var oriVertex = createEqualAngles();
+		var blb = new GeneralizedBigLittleBigLemma();
+
+		assertTrue(blb.holds(oriVertex));
+	}
+
+	private OriVertex createTwoSequences() {
+		var oriVertex = new OriVertex(1, 1);
+		oriVertex.addEdge(createEdge(0, 0, 1, 1, OriLine.Type.MOUNTAIN));
+		oriVertex.addEdge(createEdge(1, 0, 1, 1, OriLine.Type.VALLEY));
+		oriVertex.addEdge(createEdge(2, 0, 1, 1, OriLine.Type.MOUNTAIN));
+
+		oriVertex.addEdge(createEdge(2, 2, 1, 1, OriLine.Type.VALLEY));
+		oriVertex.addEdge(createEdge(1, 2, 1, 1, OriLine.Type.MOUNTAIN));
+		oriVertex.addEdge(createEdge(0, 2, 1, 1, OriLine.Type.MOUNTAIN));
+
+		return oriVertex;
+	}
+
+	@Test
+	void testHolds_45deg_135deg() {
+		var oriVertex = create45deg135deg();
+		var blb = new GeneralizedBigLittleBigLemma();
+
+		assertTrue(blb.holds(oriVertex));
+	}
+
+	private OriVertex create45deg135deg() {
+		var oriVertex = new OriVertex(0, 0);
+		var angle = Math.PI / 8 * 3;
+		oriVertex.addEdge(
+				createEdge(0, 0, Math.cos(angle), Math.signum(angle), OriLine.Type.MOUNTAIN));
+		angle = Math.PI / 8 * 5;
+		oriVertex.addEdge(
+				createEdge(0, 0, Math.cos(angle), Math.signum(angle), OriLine.Type.VALLEY));
+
+		angle = Math.PI / 8 * 9;
+		oriVertex.addEdge(
+				createEdge(0, 0, Math.cos(angle), Math.signum(angle), OriLine.Type.MOUNTAIN));
+		angle = Math.PI / 8 * (-1);
+		oriVertex.addEdge(
+				createEdge(0, 0, Math.cos(angle), Math.signum(angle), OriLine.Type.MOUNTAIN));
+
+		return oriVertex;
+
+	}
+
+	private OriEdge createEdge(final double x0, final double y0, final double x1, final double y1,
+			final OriLine.Type type) {
+		return new OriEdge(new OriVertex(x0, y0), new OriVertex(x1, y1), type.toInt());
+	}
+
+}
