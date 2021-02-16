e4d3bf8a4a16f8bb7e2795eaad951331b9995e38
origami ouch <origamist.koji@gmail.com>
2021-02-16 09:54:06 +0900

    Change to package-private.

--- src/test/java/oripa/domain/fold/OriEdgeFactoryForTest.java
+++ src/test/java/oripa/domain/fold/OriEdgeFactoryForTest.java
@@ -24,7 +24,7 @@ import oripa.value.OriLine;
  * @author OUCHI Koji
  *
  */
-public class OriEdgeFactoryForTest {
+class OriEdgeFactoryForTest {
 	public static OriEdge createEdge(final double x0, final double y0,
 			final double x1, final double y1,
 			final OriLine.Type type) {
