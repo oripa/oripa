cd57a83408b43c6e97ad6806b43db5cace71495c
origami ouch <origamist.koji@gmail.com>
2021-07-08 23:05:17 +0900

    Update javadoc.

--- src/main/java/oripa/domain/paint/core/ActionState.java
+++ src/main/java/oripa/domain/paint/core/ActionState.java
@@ -4,10 +4,26 @@ import javax.vecmath.Vector2d;
 
 import oripa.domain.paint.PaintContextInterface;
 
+/**
+ * An interface for (modified) State pattern with undo. Implementation of this
+ * interface should determine the state to be used after every action/undo.
+ *
+ * @author OUCHI Koji
+ */
 public interface ActionState {
+	/**
+	 * Performs the action of this state and returns the next state.
+	 *
+	 * @return next state.
+	 */
 	public ActionState doAction(PaintContextInterface context,
 			Vector2d currentPoint, boolean differentAction);
 
+	/**
+	 * Performs undo of this state and returns the previous state.
+	 *
+	 * @return previous state.
+	 */
 	public ActionState undo(PaintContextInterface context);
 
 	public ActionState getNextState();
