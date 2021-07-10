79cb17d2ca9850356d5298a8538b0af86542b4f5
origami ouch <origamist.koji@gmail.com>
2021-07-10 20:39:43 +0900

    Extract domain job.

--- src/main/java/oripa/application/main/DeleteSelectedLinesActionListener.java
+++ src/main/java/oripa/application/main/DeleteSelectedLinesActionListener.java
@@ -3,16 +3,11 @@ package oripa.application.main;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import oripa.domain.cptool.Painter;
 import oripa.domain.paint.PaintContext;
+import oripa.domain.paint.deleteline.SelectedLineDeleter;
 import oripa.gui.presenter.creasepattern.ScreenUpdater;
 
 public class DeleteSelectedLinesActionListener implements ActionListener {
-	private static final Logger logger = LoggerFactory
-			.getLogger(DeleteSelectedLinesActionListener.class);
 	private final PaintContext context;
 	private final ScreenUpdater screenUpdater;
 
@@ -24,22 +19,10 @@ public class DeleteSelectedLinesActionListener implements ActionListener {
 
 	@Override
 	public void actionPerformed(final ActionEvent e) {
+		var deleter = new SelectedLineDeleter();
 
-		context.creasePatternUndo().pushUndoInfo();
-
-		Painter painter = context.getPainter();
-
-		try {
-			painter.removeSelectedLines();
-		} catch (Exception ex) {
-			logger.error("error when deleting selected lines", ex);
-		}
-		if (context.isPasting() == false) {
-			context.clear(false);
-		}
-
+		deleter.deleteSelectedLine(context);
 		screenUpdater.updateScreen();
-
 	}
 
 }
