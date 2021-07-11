c198b4b9f1292152a1fdece2cf7676c30157e95d
origami ouch <origamist.koji@gmail.com>
2021-07-11 19:26:31 +0900

    Extract behavior as a command.

---
+++ src/main/java/oripa/domain/paint/byvalue/AngleMeasureCommand.java
@@ -0,0 +1,56 @@
+/**
+ * ORIPA - Origami Pattern Editor
+ * Copyright (C) 2013-     ORIPA OSS Project  https://github.com/oripa/oripa
+ * Copyright (C) 2005-2009 Jun Mitani         http://mitani.cs.tsukuba.ac.jp/
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package oripa.domain.paint.byvalue;
+
+import javax.vecmath.Vector2d;
+
+import oripa.domain.paint.PaintContext;
+import oripa.util.Command;
+
+/**
+ * @author OUCHI Koji
+ *
+ */
+public class AngleMeasureCommand implements Command {
+	private final PaintContext context;
+	private final ValueSetting valueSetting;
+
+	public AngleMeasureCommand(final PaintContext context, final ValueSetting valueSetting) {
+		this.context = context;
+		this.valueSetting = valueSetting;
+	}
+
+	@Override
+	public void execute() {
+		Vector2d first = context.getVertex(0);
+		Vector2d second = context.getVertex(1);
+		Vector2d third = context.getVertex(2);
+
+		Vector2d dir1 = new Vector2d(third);
+		Vector2d dir2 = new Vector2d(first);
+		dir1.sub(second);
+		dir2.sub(second);
+
+		double deg_angle = Math.toDegrees(dir1.angle(dir2));
+
+		valueSetting.setAngle(deg_angle);
+
+		context.clear(false);
+	}
+}
--- src/main/java/oripa/domain/paint/byvalue/SelectingVertexForAngle.java
+++ src/main/java/oripa/domain/paint/byvalue/SelectingVertexForAngle.java
@@ -4,6 +4,7 @@ import javax.vecmath.Vector2d;
 
 import oripa.domain.paint.PaintContext;
 import oripa.domain.paint.core.PickingVertex;
+import oripa.util.Command;
 
 public class SelectingVertexForAngle extends PickingVertex {
 
@@ -40,20 +41,12 @@ public class SelectingVertexForAngle extends PickingVertex {
 	@Override
 	public void onResult(final PaintContext context, final boolean doSpecial) {
 
-		Vector2d first = context.getVertex(0);
-		Vector2d second = context.getVertex(1);
-		Vector2d third = context.getVertex(2);
+		if (context.getVertexCount() != 3 || context.getLineCount() != 0) {
+			throw new IllegalStateException("Wrong state: impossible selection.");
+		}
 
-		Vector2d dir1 = new Vector2d(third);
-		Vector2d dir2 = new Vector2d(first);
-		dir1.sub(second);
-		dir2.sub(second);
-
-		double deg_angle = Math.toDegrees(dir1.angle(dir2));
-
-		valueSetting.setAngle(deg_angle);
-
-		context.clear(false);
+		Command command = new AngleMeasureCommand(context, valueSetting);
+		command.execute();
 
 		context.setMissionCompleted(true);
 	}
