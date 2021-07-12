557fc600b1d13a49bdf825f22870e97028876d7e
origami ouch <origamist.koji@gmail.com>
2021-07-12 12:49:05 +0900

    Extract behavior as a command.

--- src/main/java/oripa/domain/paint/vertical/SelectingLineForVertical.java
+++ src/main/java/oripa/domain/paint/vertical/SelectingLineForVertical.java
@@ -1,10 +1,8 @@
 package oripa.domain.paint.vertical;
 
-import oripa.domain.cptool.Painter;
 import oripa.domain.paint.PaintContext;
 import oripa.domain.paint.core.PickingLine;
-import oripa.geom.GeomUtil;
-import oripa.value.OriLine;
+import oripa.util.Command;
 
 public class SelectingLineForVertical extends PickingLine {
 
@@ -23,18 +21,11 @@ public class SelectingLineForVertical extends PickingLine {
 	protected void onResult(final PaintContext context, final boolean doSpecial) {
 		if (context.getLineCount() != 1 ||
 				context.getVertexCount() != 1) {
-			throw new RuntimeException();
+			throw new IllegalStateException("Wrong state: impossible selection.");
 		}
 
-		OriLine vl = new OriLine(GeomUtil.getVerticalLine(
-				context.getVertex(0), context.getLine(0)), context.getLineTypeOfNewLines());
-
-		context.creasePatternUndo().pushUndoInfo();
-
-		Painter painter = context.getPainter();
-		painter.addLine(vl);
-
-		context.clear(false);
+		Command command = new VerticalLineAdderCommand(context);
+		command.execute();
 	}
 
 }
