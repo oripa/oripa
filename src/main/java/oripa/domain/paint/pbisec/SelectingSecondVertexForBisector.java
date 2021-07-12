7c25ff8273fff02ac49d435f8e02b374246092d9
origami ouch <origamist.koji@gmail.com>
2021-07-12 09:17:33 +0900

    Extract behavior as a command.

--- src/main/java/oripa/domain/paint/pbisec/SelectingSecondVertexForBisector.java
+++ src/main/java/oripa/domain/paint/pbisec/SelectingSecondVertexForBisector.java
@@ -1,10 +1,8 @@
 package oripa.domain.paint.pbisec;
 
-import javax.vecmath.Vector2d;
-
-import oripa.domain.cptool.Painter;
 import oripa.domain.paint.PaintContext;
 import oripa.domain.paint.core.PickingVertex;
+import oripa.util.Command;
 
 public class SelectingSecondVertexForBisector extends PickingVertex {
 
@@ -19,25 +17,14 @@ public class SelectingSecondVertexForBisector extends PickingVertex {
 			throw new IllegalStateException();
 		}
 
-		Vector2d p0, p1;
-		p0 = context.getVertex(0);
-		p1 = context.getVertex(1);
-
-		context.creasePatternUndo().pushUndoInfo();
-
-		Painter painter = context.getPainter();
-		painter.addPBisector(
-				p0, p1, context.getPaperDomain(), context.getLineTypeOfNewLines());
-
-		context.clear(false);
+		Command command = new PerpendicularBisectorAdderCommand(context);
+		command.execute();
 	}
 
 	@Override
 	protected void initialize() {
 		setPreviousClass(SelectingFirstVertexForBisector.class);
 		setNextClass(SelectingFirstVertexForBisector.class);
-
-//		System.out.println("SelectingFirstVertex.initialize() is called");
 	}
 
 }
