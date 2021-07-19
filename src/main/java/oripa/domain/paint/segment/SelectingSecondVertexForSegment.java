4bda9bc35ea7b38303792ce6324317eea2cc6253
origami ouch <origamist.koji@gmail.com>
2021-07-19 11:55:23 +0900

    Delete meaningless validation.

--- src/main/java/oripa/domain/paint/segment/SelectingSecondVertexForSegment.java
+++ src/main/java/oripa/domain/paint/segment/SelectingSecondVertexForSegment.java
@@ -12,11 +12,6 @@ public class SelectingSecondVertexForSegment extends PickingVertex {
 
 	@Override
 	protected void onResult(final PaintContext context, final boolean doSpecial) {
-
-		if (context.getVertexCount() != 2) {
-			throw new IllegalStateException("wrong state: impossible vertex selection.");
-		}
-
 		var command = new PickedVerticesConnectionLineAdderCommand(context);
 		command.execute();
 	}
