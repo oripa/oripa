843948a49c2e72229fda17cf23d35a32d57a47b6
lifesbest23 <goofy23.2@gmail.com>
2021-12-04 02:53:47 +0100

    Code moving and GridBagConstraintsBuilder usage added

--- src/main/java/oripa/gui/view/main/AbstractPropertyDialog.java
+++ src/main/java/oripa/gui/view/main/AbstractPropertyDialog.java
@@ -19,6 +19,7 @@
 package oripa.gui.view.main;
 
 import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
 
@@ -26,21 +27,18 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 import oripa.doc.Property;
+import oripa.gui.view.util.GridBagConstraintsBuilder;
 
 // TODO: Use label resource.
 public abstract class AbstractPropertyDialog extends JDialog implements
 		ComponentListener {
 
-	/**
-	 *
-	 */
 	private static final long serialVersionUID = -5864700666603644379L;
-	private JPanel jContentPane = null;
+
 	private JLabel jLabel = null;
 	public JTextField TitleTextField = null;
 	private JLabel jLabel1 = null;
@@ -55,14 +53,61 @@ public abstract class AbstractPropertyDialog extends JDialog implements
 
 	private final Property property;
 
-	/**
-	 * This is the default constructor
-	 */
 	public AbstractPropertyDialog(final JFrame parent, final Property prop) {
 		super(parent);
-		initialize();
 
-		property = prop;
+		this.property = prop;
+
+		setTitle("Model Information");
+		setSize(420, 278);
+		addComponentListener(this);
+		var contentPane = getContentPane();
+		contentPane.setLayout(new GridBagLayout());
+
+		jLabel = new JLabel();
+		jLabel.setText("Title");
+		jLabel.setComponentOrientation(java.awt.ComponentOrientation.UNKNOWN);
+		jLabel1 = new JLabel();
+		jLabel1.setText("Author data");
+		jLabel2 = new JLabel();
+		jLabel2.setText("Model creator");
+		jLabel3 = new JLabel();
+		jLabel3.setText("Source");
+		jLabel4 = new JLabel();
+		jLabel4.setText("Memo");
+
+		var leftWeight = 0.0;
+		var rightWeight = 1.0;
+
+		var gbcBuilder = new GridBagConstraintsBuilder(2).setInsets(2, 5, 2, 5).setWeight(leftWeight, 0.0)
+				.setAnchor(GridBagConstraints.CENTER);
+
+		contentPane.add(jLabel, gbcBuilder.getNextField());
+		gbcBuilder.setWeight(rightWeight, 0.0);
+		contentPane.add(getTitleTextField(), gbcBuilder.getNextField());
+
+		gbcBuilder.setWeight(leftWeight, 0.0);
+		contentPane.add(jLabel1, gbcBuilder.getNextField());
+		gbcBuilder.setWeight(rightWeight, 0.0);
+		contentPane.add(getEditorNameTextField(), gbcBuilder.getNextField());
+
+		gbcBuilder.setWeight(leftWeight, 0.0);
+		contentPane.add(jLabel2, gbcBuilder.getNextField());
+		gbcBuilder.setWeight(rightWeight, 0.0);
+		contentPane.add(getOriginalAuthorTextField(), gbcBuilder.getNextField());
+
+		gbcBuilder.setWeight(leftWeight, 0.0);
+		contentPane.add(jLabel3, gbcBuilder.getNextField());
+		gbcBuilder.setWeight(rightWeight, 0.0);
+		contentPane.add(getOriginTextField(), gbcBuilder.getNextField());
+
+		gbcBuilder.setWeight(leftWeight, 0.0);
+		contentPane.add(jLabel4, gbcBuilder.getNextField());
+		gbcBuilder.setWeight(rightWeight, 1.0).setFill(GridBagConstraints.BOTH);
+		contentPane.add(getMemoTextArea(), gbcBuilder.getNextField());
+
+		gbcBuilder.setWeight(leftWeight, 0.0).setFill(GridBagConstraints.NONE);
+		contentPane.add(getOKButton(), gbcBuilder.getLineField());
 	}
 
 	public void setValue() {
@@ -82,94 +127,6 @@ public abstract class AbstractPropertyDialog extends JDialog implements
 		prop.setMemo(MemoTextArea.getText());
 
 		return prop;
-	}
-
-	/**
-	 * This method initializes this
-	 *
-	 * @return void
-	 */
-	private void initialize() {
-		this.addComponentListener(this);
-		this.setSize(420, 278);
-		this.setContentPane(getJContentPane());
-		this.setTitle("Model Information");
-	}
-
-	/**
-	 * This method initializes jContentPane
-	 *
-	 * @return javax.swing.JPanel
-	 */
-	private JPanel getJContentPane() {
-		if (jContentPane == null) {
-			GridBagConstraints gridBagConstraints8 = new GridBagConstraints();
-			gridBagConstraints8.gridx = 1;
-			gridBagConstraints8.gridy = 5;
-			GridBagConstraints gridBagConstraints6 = new GridBagConstraints();
-			gridBagConstraints6.fill = java.awt.GridBagConstraints.BOTH;
-			gridBagConstraints6.gridy = 4;
-			gridBagConstraints6.weightx = 1.0;
-			gridBagConstraints6.weighty = 1.0;
-			gridBagConstraints6.gridx = 1;
-			GridBagConstraints gridBagConstraints5 = new GridBagConstraints();
-			gridBagConstraints5.fill = java.awt.GridBagConstraints.HORIZONTAL;
-			gridBagConstraints5.gridy = 3;
-			gridBagConstraints5.weightx = 1.0;
-			gridBagConstraints5.gridx = 1;
-			GridBagConstraints gridBagConstraints4 = new GridBagConstraints();
-			gridBagConstraints4.fill = java.awt.GridBagConstraints.HORIZONTAL;
-			gridBagConstraints4.gridy = 2;
-			gridBagConstraints4.weightx = 1.0;
-			gridBagConstraints4.gridx = 1;
-			GridBagConstraints gridBagConstraints3 = new GridBagConstraints();
-			gridBagConstraints3.fill = java.awt.GridBagConstraints.HORIZONTAL;
-			gridBagConstraints3.gridy = 1;
-			gridBagConstraints3.weightx = 1.0;
-			gridBagConstraints3.gridx = 1;
-			GridBagConstraints gridBagConstraints21 = new GridBagConstraints();
-			gridBagConstraints21.gridx = 0;
-			gridBagConstraints21.gridy = 4;
-			jLabel4 = new JLabel();
-			jLabel4.setText("Memo");
-			GridBagConstraints gridBagConstraints11 = new GridBagConstraints();
-			gridBagConstraints11.gridx = 0;
-			gridBagConstraints11.gridy = 3;
-			jLabel3 = new JLabel();
-			jLabel3.setText("Source");
-			GridBagConstraints gridBagConstraints2 = new GridBagConstraints();
-			gridBagConstraints2.gridx = 0;
-			gridBagConstraints2.gridy = 2;
-			jLabel2 = new JLabel();
-			jLabel2.setText("Model creator");
-			GridBagConstraints gridBagConstraints1 = new GridBagConstraints();
-			gridBagConstraints1.gridx = 0;
-			gridBagConstraints1.gridy = 1;
-			jLabel1 = new JLabel();
-			jLabel1.setText("Author data");
-			GridBagConstraints gridBagConstraints = new GridBagConstraints();
-			gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
-			gridBagConstraints.gridy = 0;
-			gridBagConstraints.weightx = 1.0;
-			gridBagConstraints.gridx = 1;
-			jLabel = new JLabel();
-			jLabel.setText("Title");
-			jLabel.setComponentOrientation(java.awt.ComponentOrientation.UNKNOWN);
-			jContentPane = new JPanel();
-			jContentPane.setLayout(new java.awt.GridBagLayout());
-			jContentPane.add(getOKButton(), gridBagConstraints8);
-			jContentPane.add(jLabel, null);
-			jContentPane.add(jLabel1, gridBagConstraints1);
-			jContentPane.add(jLabel2, gridBagConstraints2);
-			jContentPane.add(jLabel3, gridBagConstraints11);
-			jContentPane.add(jLabel4, gridBagConstraints21);
-			jContentPane.add(getTitleTextField(), gridBagConstraints);
-			jContentPane.add(getEditorNameTextField(), gridBagConstraints3);
-			jContentPane.add(getOriginalAuthorTextField(), gridBagConstraints4);
-			jContentPane.add(getOriginTextField(), gridBagConstraints5);
-			jContentPane.add(getMemoTextArea(), gridBagConstraints6);
-		}
-		return jContentPane;
 	}
 
 	/**
