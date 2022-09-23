ebd0179da690d0ca645f00d8f8641e0505f13864
origami ouch <origamist.koji@gmail.com>
2022-09-23 09:43:28 +0900

    Add suggestion algorithm.

---
+++ src/main/java/oripa/domain/suggestion/KawasakiTheoremSuggester.java
@@ -0,0 +1,128 @@
+/**
+ * ORIPA - Origami Pattern Editor
+ * Copyright (C) 2013-     ORIPA OSS Project  https://github.com/oripa/oripa
+ * Copyright (C) 2005-2009 Jun Mitani         http://mitani.cs.tsukuba.ac.jp/
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package oripa.domain.suggestion;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import oripa.domain.cptool.PseudoRayFactory;
+import oripa.domain.fold.halfedge.OriVertex;
+import oripa.domain.fold.origeom.OriGeomUtil;
+import oripa.value.OriLine;
+
+/**
+ * @author OUCHI Koji
+ *
+ */
+public class KawasakiTheoremSuggester {
+
+	private final double paperSize;
+
+	public KawasakiTheoremSuggester(final double paperSize) {
+		this.paperSize = paperSize;
+	}
+
+	public Collection<OriLine> suggest(final OriVertex vertex) {
+
+		int angleCount = vertex.edgeCount();
+
+		if (angleCount % 2 == 0) {
+			return List.of();
+		}
+
+		var suggestions = new ArrayList<OriLine>();
+		var type = guessType(vertex);
+
+		if (type == null) {
+			return List.of();
+		}
+
+		double evenSum = 0;
+
+		for (int i = 0; i < angleCount; i += 2) {
+			evenSum += OriGeomUtil.getAngleDifference(vertex, i);
+		}
+
+		double oddSum = 2 * Math.PI - evenSum;
+
+		evenSum -= OriGeomUtil.getAngleDifference(vertex, 0);
+
+		// computes as if indices are re-assigned as current i-th is the new
+		// 0th.
+		for (var i = 0; i < angleCount; i++) {
+			double theta = OriGeomUtil.getAngleDifference(vertex, i);
+			double t = evenSum + theta - Math.PI;
+
+			if (evenSum < Math.PI && oddSum < Math.PI) {
+				var baseAngle = vertex.getEdge(i).getAngle(vertex);
+				var factory = new PseudoRayFactory();
+				var segment = factory.create(vertex.getPositionBeforeFolding(), baseAngle + t, paperSize);
+				suggestions.add(new OriLine(segment, type));
+			}
+
+			var tmpEvenSum = evenSum;
+			evenSum = oddSum + theta - OriGeomUtil.getAngleDifference(vertex, i + 1);
+			oddSum = tmpEvenSum;
+		}
+
+		return suggestions;
+
+//				b = 2*PI - a
+//				a = a - theta_0
+//
+//				for i = 0; i < n; i++
+//				  t = a + theta_i - PI
+//				  if a < PI and b < PI
+//				    l = a new line between l_i and l_(i+1) as the angle between l_i is t.
+//				    add l to K
+//				  a_tmp = a
+//				  a = b + theta_i - theta_(i+1)
+//				  b = a_tmp
+//				return K
+
+	}
+
+	private OriLine.Type guessType(final OriVertex vertex) {
+		double edgeCount = vertex.edgeCount();
+		int mountainCount = 0;
+		int valleyCount = 0;
+
+		for (int i = 0; i < edgeCount; i++) {
+			if (vertex.getEdge(i).isMountain()) {
+				mountainCount++;
+			} else {
+				valleyCount++;
+			}
+		}
+		int diff = mountainCount - valleyCount;
+		switch (diff) {
+		case 3:
+			return OriLine.Type.VALLEY;
+		case -3:
+			return OriLine.Type.MOUNTAIN;
+		case 1:
+			return OriLine.Type.MOUNTAIN;
+		case -1:
+			return OriLine.Type.VALLEY;
+		default:
+			return null;
+		}
+	}
+}
---
+++ src/test/java/oripa/domain/suggestion/KawasakiTheoremSuggesterTest.java
@@ -0,0 +1,89 @@
+package oripa.domain.suggestion;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import javax.vecmath.Vector2d;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import oripa.domain.fold.halfedge.OriEdge;
+import oripa.domain.fold.halfedge.OriVertex;
+import oripa.value.OriLine;
+
+class KawasakiTheoremSuggesterTest {
+	private final double UNIT_ANGLE = Math.PI / 8;
+
+	private static Logger logger = LoggerFactory.getLogger(KawasakiTheoremSuggesterTest.class);
+
+	@Test
+	void test_3InputLines() {
+		var vertex = new OriVertex(0, 0);
+
+		var edges = List.of(
+				createEdge(vertex, 1, OriLine.Type.MOUNTAIN),
+				createEdge(vertex, 8, OriLine.Type.MOUNTAIN),
+				createEdge(vertex, 13, OriLine.Type.MOUNTAIN));
+
+		edges.forEach(vertex::addEdge);
+
+		var suggester = new KawasakiTheoremSuggester(1);
+
+		var suggestions = suggester.suggest(vertex);
+
+		assertSame(OriLine.Type.VALLEY, suggestions.stream().findFirst().get().getType());
+
+		var expectedLines = List.of(
+				createLine(vertex, 4, OriLine.Type.VALLEY),
+				createLine(vertex, 12, OriLine.Type.VALLEY),
+				createLine(vertex, 14, OriLine.Type.VALLEY));
+
+		expectedLines.forEach(expected -> assertTrue(
+				suggestions.stream()
+						.anyMatch(line -> angleEquals(line, expected, vertex))));
+	}
+
+	private boolean angleEquals(final OriLine l1, final OriLine l2, final OriVertex center) {
+
+		var angle1 = getAngle(l1, center);
+		var angle2 = getAngle(l2, center);
+
+		logger.debug("angle1: {}, angle2: {}", angle1, angle2);
+
+		return Math.abs(angle1 - angle2) < 1e-5;
+	}
+
+	private double getAngle(final OriLine line, final OriVertex center) {
+		Vector2d dir;
+		if (line.getP0().epsilonEquals(center.getPosition(), 1e-5)) {
+			dir = line.getP1();
+			dir.sub(line.getP0());
+		} else {
+			dir = line.getP0();
+			dir.sub(line.getP1());
+		}
+
+		return Math.atan2(dir.getY(), dir.getX());
+	}
+
+	private OriEdge createEdge(final OriVertex start, final int angle, final OriLine.Type type) {
+		return new OriEdge(
+				start,
+				new OriVertex(
+						createPoint(start, angle)),
+				type.toInt());
+	}
+
+	private OriLine createLine(final OriVertex start, final int angle, final OriLine.Type type) {
+		return new OriLine(start.getPosition(), createPoint(start, angle), type);
+	}
+
+	private Vector2d createPoint(final OriVertex start, final int angle) {
+		return new Vector2d(
+				start.getPosition().getX() + Math.cos(UNIT_ANGLE * angle),
+				start.getPosition().getY() + Math.sin(UNIT_ANGLE * angle));
+	}
+}
