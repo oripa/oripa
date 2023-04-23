bf0fd592add431e85881aa2767d8236621d0fb45
origami ouch <origamist.koji@gmail.com>
2023-04-23 18:02:48 +0900

    Increase .opx major version.

--- src/main/java/oripa/resource/Version.java
+++ src/main/java/oripa/resource/Version.java
@@ -3,7 +3,7 @@ package oripa.resource;
 public class Version {
 	public static final String ORIPA_VERSION = "1.64";
 
-	public static final int FILE_MAJOR_VERSION = 1;
+	public static final int FILE_MAJOR_VERSION = 2;
 	public static final int FILE_MINOR_VERSION = 1;
 
 }
