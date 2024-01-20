df0ad95b369ba00f20a7737b09f2597314a50bf5
origami ouch <origamist.koji@gmail.com>
2024-01-21 08:09:08 +0900

    Prepare immutable vector2d.

---
+++ src/main/java/oripa/vecmath/Vector2d.java
@@ -0,0 +1,103 @@
+/**
+ * ORIPA - Origami Pattern Editor
+ * Copyright (C) 2013-     ORIPA OSS Project  https://github.com/oripa/oripa
+ * Copyright (C) 2005-2009 Jun Mitani         http://mitani.cs.tsukuba.ac.jp/
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package oripa.vecmath;
+
+import java.util.Objects;
+
+/**
+ * @author OUCHI Koji
+ *
+ */
+public class Vector2d {
+
+	private final double x;
+	private final double y;
+
+	public Vector2d(final double x, final double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public Vector2d(final Vector2d v) {
+		this(v.x, v.y);
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	/**
+	 * intentionally violates naming convention to make migration from
+	 * javax.vecmath easy.
+	 *
+	 * @param v
+	 * @return this + v
+	 */
+	public Vector2d addition(final Vector2d v) {
+		return new Vector2d(x + v.x, y + v.y);
+	}
+
+	public Vector2d subtract(final Vector2d v) {
+		return new Vector2d(x - v.x, y - v.y);
+	}
+
+	public Vector2d multiply(final double a) {
+		return new Vector2d(a * x, a * y);
+	}
+
+	public double length() {
+		return Math.sqrt(x * x + y * y);
+	}
+
+	public double lengthSquared() {
+		return x * x + y * y;
+	}
+
+	public Vector2d normalization() {
+		return multiply(1.0 / length());
+	}
+
+	public double dot(final Vector2d v) {
+		return x * v.x + y * v.y;
+	}
+
+	public double angle(final Vector2d v) {
+		return dot(v) / (length() * v.length());
+	}
+
+	@Override
+	public boolean equals(final Object o) {
+		if (!(o instanceof Vector2d)) {
+			return false;
+		}
+
+		var v = (Vector2d) o;
+
+		return x == v.x && y == v.y;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(x, y);
+	}
+}
