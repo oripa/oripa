56cf7f20f3d4dd4bca67ff0c9548bbccc8a40123
gera <gerasdf@>
2025-10-28 08:54:25 +0100

    maintaining contour from existing CP.
    Useful when the user has edited contour

--- src/main/java/oripa/domain/creasepattern/CreasePatternFactory.java
+++ src/main/java/oripa/domain/creasepattern/CreasePatternFactory.java
@@ -104,9 +104,16 @@ public class CreasePatternFactory {
 	 * existing crease pattern. Only boundary will be recreated; lines are not
 	 * copied by this method.
 	 */
-	public CreasePattern createCreasePatternFromContourOf(final CreasePattern existing) {
-		var domain = existing.getPaperDomain();
-		return createCreasePattern(domain.getWidth(), domain.getHeight());
+	public CreasePattern createCreasePatternFromContourOf(final CreasePattern existingCP) {
+		var domain = existingCP.getPaperDomain();
+		List<OriLine> outlines = existingCP.stream()
+				.filter(OriLine::isBoundary).toList();
+
+		CreasePattern creasePattern = new CreasePatternImpl(domain);
+
+		creasePattern.addAll(outlines);
+
+		return creasePattern;
 	}
 
 }
